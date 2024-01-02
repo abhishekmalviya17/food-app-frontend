81e5de28b512ac4ab218f362a19fe00743a87691
abhishekmalviya17 <abhishekmalviya17@gmail.com>
2024-01-02 09:43:29 +0530

    changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: process.env.NODE_ENV === 'developmentp' ?  'https://food-delivery-app-abhi-260ef83a472a.herokuapp.com' : 'http://localhost:3000'
+    apiUrl: process.env.NODE_ENV === 'development' ?  'https://food-delivery-app-abhi-260ef83a472a.herokuapp.com' : 'http://localhost:3000'
 };
 
 export default config;
